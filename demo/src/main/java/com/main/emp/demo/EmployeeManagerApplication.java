f128c5a6b7989d978a41fd5639a06666ea016a6b
visels <elviskipchumba@yahoo.com>
2022-11-25 22:23:02 +0300

    project initialization 2

--- demo/src/main/java/com/main/emp/demo/EmployeeManagerApplication.java
+++ demo/src/main/java/com/main/emp/demo/EmployeeManagerApplication.java
@@ -1,13 +1,15 @@
 package com.main.emp.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class EmployeeManagerApplication {
-
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(EmployeeManagerApplication.class, args);
 	}
-
 }
