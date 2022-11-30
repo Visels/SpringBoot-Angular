4e30994534d85db59ef40a10716c959d3ad9c0c5
visels <elviskipchumba@yahoo.com>
2022-11-30 14:07:40 +0300

    employee service configured

--- employeeManagerApp/src/app/app.component.ts
+++ employeeManagerApp/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'employeeManagerApp';
+
+   public employees!: Employee[];
+
+   constructor(private employeeService: EmployeeService){};
+
 }
